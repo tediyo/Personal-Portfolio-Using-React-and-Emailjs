2a7b193d4e9d0327f63e71884d69018d0542bced
tediyo <thedronberhanu05@gmail.com>
2023-11-24 14:58:53 -0800

    update002

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Tewodros Berhanu. I'm a web designer and developer based in Addis Abeba, Ethiopia.",
-    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
+    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs,  React,  Redux, Bootstrap, UX/UI Design, Material UI, MongoDB, Mysql, etc.",
 }
