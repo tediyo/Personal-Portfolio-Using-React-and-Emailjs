21cd050350fa13147dce3cd13d56173eca7d520b
tediyo <thedronberhanu05@gmail.com>
2024-02-23 13:07:52 -0900

    contact modification

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'thedronberhanu16@gmail.com',
-    phone: '+251947087598',
-    address: 'Addis Abeba ,Ethiopia ',
+    phone: '+251947087598/251930448665',
+    address: 'Addis Ababa ,Ethiopia ',
     github: 'https://github.com/tediyo',
     facebook: '',
     linkedIn: 'https://www.linkedin.com/in/tewodros-berhanu-953750230',
