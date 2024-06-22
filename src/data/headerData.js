cee1449872b99b391452f35f73df3571089b9185
tediyo <thedronberhanu05@gmail.com>
2024-06-21 22:38:28 -0800

    Image Localization

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,11 +1,11 @@
 import resume from "../assets/pdf/resume.pdf";
 
-
+import Teddy_Github from "../assets/png/Teddy_Github.jpg"
 export const headerData = {
     name: 'Tewodros Berhanu',
     title: "ppppp",
     desciption: "Hello! I am Tewodros , a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://ibb.co/S69wmJW/Teddy-Github.jpg',
+    image: Teddy_Github.jpg,
     //imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: resume,
 }
