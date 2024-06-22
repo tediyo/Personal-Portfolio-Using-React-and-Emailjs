944adabfa3fcbb288c5033bec04a1b0ae1fbe974
tediyo <thedronberhanu05@gmail.com>
2024-06-21 22:57:07 -0800

    Bio

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import Teddy_Github from "../assets/png/Teddy_Github.jpg"
 export const headerData = {
     name: 'Tewodros Berhanu',
     title: "ppppp",
-    desciption: "Hi 👋, I'm Tewodros, a passionate and dedicated Junior Software Developer with a strong foundation in software engineering principles and a keen interest in developing innovative and efficient solutions. My journey in software development began during my academic years, where I honed my skills in various programming languages and technologies.",
+    desciption: "Hi 👋, I'm Tewodros, a passionate and dedicated  Software Developer with a strong foundation in software engineering principles and a keen interest in developing innovative and efficient solutions. My journey in software development began during my academic years, where I honed my skills in various programming languages and technologies.",
     image: Teddy_Github,
     //imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: resume,
