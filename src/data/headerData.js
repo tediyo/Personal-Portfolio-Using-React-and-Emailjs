5d8f21f337ea11b4ba0d76ae843d3aecbf65f9f9
tediyo <thedronberhanu05@gmail.com>
2024-06-22 06:35:12 -0800

    Last for Today

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,6 +6,6 @@ export const headerData = {
     title: "ppppp",
     desciption: "Hi 👋, I'm Tewodros, a passionate and dedicated Software Developer with a strong foundation in software engineering principles and a keen interest in developing innovative and efficient solutions. My journey in software development began during my academic years, where I honed my skills in various programming languages and technologies.",
     image: Teddy_Github,
-    //imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
+    
     resumePdf: resume,
 }
