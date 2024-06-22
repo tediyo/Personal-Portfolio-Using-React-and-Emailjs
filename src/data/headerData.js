5aa3d487139697e1483471e25ffbb8730665a52d
tediyo <thedronberhanu05@gmail.com>
2024-06-21 22:21:44 -0800

    Image uploded

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,7 +5,7 @@ export const headerData = {
     name: 'Tewodros Berhanu',
     title: "ppppp",
     desciption: "Hello! I am Tewodros , a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/cF9ZDHZ/aaab.jpg',
+    image: 'https://ibb.co/S69wmJW/Teddy Github.jpg',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: resume,
 }
