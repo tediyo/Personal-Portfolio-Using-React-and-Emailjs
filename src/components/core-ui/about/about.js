6da46dc3c048c779af59e384410903b221e75509
tediyo <thedronberhanu05@gmail.com>
2024-07-24 00:15:48 +0300

    About coloring

--- src/components/core-ui/about/about.js
+++ src/components/core-ui/about/about.js
@@ -20,7 +20,7 @@ function About() {
                 <div className="about-body">
                     <div className="about-description">
                         <h2 style={{ color: theme.primary }}>{aboutData.title}</h2>
-                        <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description2}</p>
+                        <p style={{ color: theme.primary }}>{aboutData.description1}<br /><br />{aboutData.description2}</p>
                     </div>
                     <div className="about-animation">
                         <AnimationLottie animationPath={codings} />
