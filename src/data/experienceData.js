3c1cf5cb02bacff8ce8da249e1ee10c3dcf22f47
thedron16 <tewodros.berhanu@walletbirr.com>
2024-11-08 10:35:58 +0300

    Experiance data updated

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,46 +1,48 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Hance Tecnologies',
-        jobtitle: 'Front End Developer',
-        jobKind: 'Hybrid',
-        startYear: 'Jun 2024',
+        company: 'Wallet Birr | Binget Group',
+        jobtitle: 'Senior Frontend Developer and QA Tester',
+        jobKind: 'Full time',
+        startYear: 'Jul 2024',
         endYear: 'Present'
     },
     {
         id: 2,
+        company: 'Hance Tecnologies',
+        jobtitle: 'Front End Developer',
+        jobKind: 'Hybrid',
+        startYear: 'Feb 2024',
+        endYear: 'Present'
+    },
+    {
+        id: 3,
         company: 'Game Changers Consulting PLC',
         jobtitle: 'Software Engineer | Project Coordinator',
         jobKind: 'Full Time',
         startYear: 'Sep 2023',
-        endYear: 'Present'
+        endYear: 'Jul 2024'
     },
    
     {
-        id: 3,
-        company: 'Mudie Ecommerce Solutions PLC',
-        jobtitle: 'Full Stack Developer ',
+        id: 4,
+        company: 'Zitec',
+        jobtitle: 'Software Developer, Technical Project Lead and QA Tester',
         jobKind: 'Remote',
-        startYear: 'Aug 2022',
+        startYear: 'Aug 2021',
         endYear: 'Sep 2023'
     },
     {
-        id: 4,
+        id: 5,
         company: 'Amhara Media Corporation',
         jobtitle: 'Software Developer | Quality Assurance Tester Intern',
         jobKind: 'Full Time Intern',
-        startYear: 'May 2022',
-        endYear: 'Oct 2022'
+        startYear: 'May 2021',
+        endYear: 'Oct 2021'
     }
-    /*
-    {
-        id: 4,
-        company: 'Upwork(freelance)',
-        jobtitle: 'Full-Stack Developer',
-        startYear: 'Jun 2022',
-        endYear: 'Present'
-    },
-    {
+    
+    
+    /*{
         id: 5,
         company: 'Learning New Technology For Everyday',
         jobtitle: 'Self Learning',
